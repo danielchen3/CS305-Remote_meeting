1c999b3de5394765f44d1d55b658b34079d7e59b
danielchen3 <daniel.cx.chen@gmail.com>
2024-11-21 21:30:41 +0800

    Update project structure

--- conf_client.py
+++ conf_client.py
@@ -2,16 +2,22 @@ from util import *
 
 
 class ConferenceClient:
-    def __init__(self,):
+    def __init__(
+        self,
+    ):
         # sync client
         self.is_working = True
         self.server_addr = None  # server addr
         self.on_meeting = False  # status
-        self.conns = None  # you may need to maintain multiple conns for a single conference
+        self.conns = (
+            None  # you may need to maintain multiple conns for a single conference
+        )
         self.support_data_types = []  # for some types of data
         self.share_data = {}
 
-        self.conference_info = None  # you may need to save and update some conference_info regularly
+        self.conference_info = (
+            None  # you may need to save and update some conference_info regularly
+        )
 
         self.recv_data = None  # you may need to save received streamd data from other clients in conference
 
@@ -39,42 +45,44 @@ class ConferenceClient:
         """
         pass
 
-    def keep_share(self, data_type, send_conn, capture_function, compress=None, fps_or_frequency=30):
-        '''
+    def keep_share(
+        self, data_type, send_conn, capture_function, compress=None, fps_or_frequency=30
+    ):
+        """
         running task: keep sharing (capture and send) certain type of data from server or clients (P2P)
         you can create different functions for sharing various kinds of data
-        '''
+        """
         pass
 
     def share_switch(self, data_type):
-        '''
+        """
         switch for sharing certain type of data (screen, camera, audio, etc.)
-        '''
+        """
         pass
 
     def keep_recv(self, recv_conn, data_type, decompress=None):
-        '''
+        """
         running task: keep receiving certain type of data (save or output)
         you can create other functions for receiving various kinds of data
-        '''
+        """
 
     def output_data(self):
-        '''
+        """
         running task: output received stream data
-        '''
+        """
 
     def start_conference(self):
-        '''
+        """
         init conns when create or join a conference with necessary conference_info
         and
         start necessary running task for conference
-        '''
+        """
 
     def close_conference(self):
-        '''
+        """
         close all conns to servers or other clients and cancel the running tasks
         pay attention to the exception handling
-        '''
+        """
 
     def start(self):
         """
@@ -82,32 +90,36 @@ class ConferenceClient:
         """
         while True:
             if not self.on_meeting:
-                status = 'Free'
+                status = "Free"
             else:
-                status = f'OnMeeting-{self.conference_id}'
+                status = f"OnMeeting-{self.conference_id}"
 
             recognized = True
-            cmd_input = input(f'({status}) Please enter a operation (enter "?" to help): ').strip().lower()
+            cmd_input = (
+                input(f'({status}) Please enter a operation (enter "?" to help): ')
+                .strip()
+                .lower()
+            )
             fields = cmd_input.split(maxsplit=1)
             if len(fields) == 1:
-                if cmd_input in ('?', '？'):
+                if cmd_input in ("?", "？"):
                     print(HELP)
-                elif cmd_input == 'create':
+                elif cmd_input == "create":
                     self.create_conference()
-                elif cmd_input == 'quit':
+                elif cmd_input == "quit":
                     self.quit_conference()
-                elif cmd_input == 'cancel':
+                elif cmd_input == "cancel":
                     self.cancel_conference()
                 else:
                     recognized = False
             elif len(fields) == 2:
-                if fields[0] == 'join':
+                if fields[0] == "join":
                     input_conf_id = fields[1]
                     if input_conf_id.isdigit():
                         self.join_conference(input_conf_id)
                     else:
-                        print('[Warn]: Input conference ID must be in digital form')
-                elif fields[0] == 'switch':
+                        print("[Warn]: Input conference ID must be in digital form")
+                elif fields[0] == "switch":
                     data_type = fields[1]
                     if data_type in self.share_data.keys():
                         self.share_switch(data_type)
@@ -117,9 +129,9 @@ class ConferenceClient:
                 recognized = False
 
             if not recognized:
-                print(f'[Warn]: Unrecognized cmd_input {cmd_input}')
+                print(f"[Warn]: Unrecognized cmd_input {cmd_input}")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     client1 = ConferenceClient()
     client1.start()
---
+++ main_server.py
@@ -0,0 +1,57 @@
+import asyncio
+from util import *
+from conf_server import ConferenceServer
+
+
+class MainServer:
+    def __init__(self, server_ip, main_port):
+        # async server
+        self.server_ip = server_ip
+        self.server_port = main_port
+        self.main_server = None
+
+        self.conference_conns = None
+        self.conference_servers = (
+            {}
+        )  # self.conference_servers[conference_id] = ConferenceManager
+
+    def handle_creat_conference(
+        self,
+    ):
+        """
+        create conference: create and start the corresponding ConferenceServer, and reply necessary info to client
+        """
+
+    def handle_join_conference(self, conference_id):
+        """
+        join conference: search corresponding conference_info and ConferenceServer, and reply necessary info to client
+        """
+
+    def handle_quit_conference(self):
+        """
+        quit conference (in-meeting request & or no need to request)
+        """
+        pass
+
+    def handle_cancel_conference(self):
+        """
+        cancel conference (in-meeting request, a ConferenceServer should be closed by the MainServer)
+        """
+        pass
+
+    async def request_handler(self, reader, writer):
+        """
+        running task: handle out-meeting (or also in-meeting) requests from clients
+        """
+        pass
+
+    def start(self):
+        """
+        start MainServer
+        """
+        pass
+
+
+if __name__ == "__main__":
+    server = MainServer(SERVER_IP, MAIN_SERVER_PORT)
+    server.start()
