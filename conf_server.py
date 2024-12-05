1f74ac92f0d02850fbc20e214658bf7ec2fd69f7
WITHOUTNAMESES <3089331088@qq.com>
2024-12-05 16:58:56 +0800

    commit

--- conf_server.py
+++ conf_server.py
@@ -5,11 +5,11 @@ import json
 class ConferenceServer:
     def __init__(self, conference_id, reader_main, writer_main):
         self.conference_id = conference_id
-        self.conf_serve_ports = None
+        self.conf_serve_ports = 8887
         self.data_serve_ports = {}
         self.data_types = ['screen', 'camera', 'audio']
-        self.clients_info = {}
-        self.client_conns = {}
+        self.reader_list = {reader_main}
+        self.writer_list = {writer_main}
         self.reader_main = reader_main
         self.writer_main = writer_main
         self.running = True
@@ -20,14 +20,15 @@ class ConferenceServer:
             if not data:
                 break
             # 将数据转发给其他所有客户端
-            for client_id, conn in self.client_conns.items():
+            for client_id, conn in self.writer_list.items():
                 if client_id != writer.get_extra_info('client_id'):
                     await conn.write(f"{data_type}:{data}".encode())
+        pass
 
     async def handle_client(self, reader, writer):
         client_id = writer.get_extra_info('client_id')
-        self.clients_info[client_id] = writer
-        self.client_conns[client_id] = writer
+        self.reader_list[client_id] = writer
+        self.writer_list[client_id] = writer
         while self.running:
             data = await reader.read(100)
             message = data.decode()
@@ -44,28 +45,30 @@ class ConferenceServer:
                 loop = asyncio.get_event_loop()
                 loop.create_task(self.handle_data(reader, writer, 'screen'))
             elif message.startswith('quit'):
-                del self.clients_info[client_id]
-                del self.client_conns[client_id]
+                del self.reader_list[client_id]
+                del self.writer_list[client_id]
                 break
             print(f"Received message from {client_id}: {message}")
+        pass
 
     async def log(self):
         while self.running:
-            print(f'Server {self.conference_id} status: {len(self.clients_info)} clients connected')
-            await asyncio.sleep(1000)
+            print(f'Server {self.conference_id} status: {len(self.reader_list)} clients connected')
+            await asyncio.sleep(100)
 
     async def cancel_conference(self):
         self.running = False
-        for conn in self.client_conns.values():
+        for conn in self.writer_list.values():
             conn.close()
         await asyncio.sleep(1)  # 等待连接关闭
         del self.main_server.conference_servers[self.conference_id]
 
-    def start(self):
+    async def start(self):
+        print("start")
         loop = asyncio.get_event_loop()
         loop.create_task(self.log())
         loop.create_task(self.accept_clients())
 
     async def accept_clients(self):
-        server = await asyncio.start_server(self.handle_client, '0.0.0.0', self.conf_serve_ports)
+        server = await asyncio.start_server(self.handle_client, '127.0.0.1', self.conf_serve_ports)
         await server.serve_forever()
