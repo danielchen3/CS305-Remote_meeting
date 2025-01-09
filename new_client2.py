ee4203e90e7ae578392d0d93eb60e9044a739a24
Daniel Chen <134826943+danielchen3@users.noreply.github.com>
2025-01-09 14:29:33 +0800

    Delete new_client2.py

--- new_client2.py
+++
@@ -1,9 +0,0 @@
-import asyncio
-import threading
-
-from new_ui import APP
-
-if __name__ == "__main__":
-    app = APP("8889","2")
-    print("start_connection")
-    app.start("1", '10.27.96.101', "8887", "2", '10.27.96.101', "8889")
